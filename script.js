c431b01f30d6f7c28cfd85a025374a64060bfb10
Israel Ben Zeev <ibz0502291330@gmail.com>
2024-12-24 17:15:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ document.getElementById('cityInput').addEventListener('keypress', function(event
 });
 
 // שולח את המידע על הביקור לשרת
-fetch('https://9fa3-147-236-108-36.ngrok-free.app/track-visit', {
+fetch('https://e285-147-236-108-36.ngrok-free.app', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
